b6593ca112a0fef08e50413567249695837d9663
chiranz <chiranpoudyal@gmail.com>
2021-07-30 11:09:37 +0530

    updated ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { joinClasses } from "../utils";
 
 const IndexPage = () => (
-  <div className={joinClasses("mt-10", "text-4xl")}>ThorScan Body</div>
+  <div className={joinClasses("mt-10", "text-4xl")}>
+    Body searching for parts
+  </div>
 );
 
 export default IndexPage;
